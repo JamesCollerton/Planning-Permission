6bdbd5f2a307526d6a9847f10a7463b1ea209cf5
James Collerton <James.Collerton@kobaltmusic.com>
2018-12-14 15:30:07 +0000

    Got part one working, need to clean up and test

--- src/main/scala/exerciseone/ExerciseOne.scala
+++ src/main/scala/exerciseone/ExerciseOne.scala
@@ -6,7 +6,8 @@ import org.apache.spark.{SparkConf, SparkContext}
 /**
   * Useful tutorial: https://hortonworks.com/tutorial/setting-up-a-spark-development-environment-with-scala/
   * Useful tutorial: https://spark.apache.org/docs/latest/sql-getting-started.html
-  * When we get null in Chmod https://stackoverflow.com/questions/40764807/null-entry-in-command-string-exception-in-saveastextfile-on-pyspark
+  * Useful tutorial: https://spark.apache.org/docs/latest/quick-start.html#self-contained-applications
+  * When we get null in Chmod https://stackoverflow.com/questions/35652665/java-io-ioexception-could-not-locate-executable-null-bin-winutils-exe-in-the-ha
   *
   *
   * Discover the schema of the input dataset and output it to a file.
@@ -15,10 +16,13 @@ object ExerciseOne {
 
   def main(args: Array[String]): Unit= {
 
-    val spark = SparkSession.builder.appName("Discover Schema").getOrCreate()
+    System.setProperty("hadoop.home.dir", "C:\\Program Files\\Hadoop")
 
-    spark.read.json("src/main/resources/planning-applications-weekly-list.json")
+    val spark = SparkSession.builder.appName("Discover Schema").config("spark.master", "local").getOrCreate()
 
+    val planningApplicationsWeekly = spark.read.json("src/main/resources/planning-applications-weekly-list.json")
+
+    planningApplicationsWeekly.schema
 
   }
 
