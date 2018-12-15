d2a6a1beb3e304b8d01647ba3e86a2b1db69008a
James Collerton <jamescollerton@live.co.uk>
2018-12-15 11:07:19 +0000

    Added test json skeletons

---
+++ src/main/scala/utilities/SparkSessionProvider.scala
@@ -0,0 +1,9 @@
+package utilities
+
+import org.apache.spark.sql.SparkSession
+
+object SparkSessionProvider {
+
+  def buildSession(sessionName: String): SparkSession = ???
+
+}
---
+++ src/test/scala/utilities/SparkSessionProviderTest.scala
@@ -0,0 +1,15 @@
+package utilities
+
+import org.scalatest.FunSuite
+
+class SparkSessionProviderTest extends FunSuite {
+
+  test("Given valid session name provided, when build session, then returns valid session") {
+
+  }
+
+  test("Given invalid session name provided, when build session, then throws exception") {
+
+  }
+
+}
